5316656ada4f90738603d45c4fd034af62913b47
francis2005Aa <francisbadeniyi@gmail.com>
2024-07-15 20:14:07 -0400

    enecryt and decrpyt plain text

--- prodigy_CS_01/casesar_cipher.py
+++ prodigy_CS_01/casesar_cipher.py
@@ -1,71 +1,55 @@
-
-# ASCII upper char = 65-90
-# ASCII lower char = 97-122
-# user should enter the mode encrpyt or decrpyt
-# handle argpase errors
-# use pyfiglet
-import  argparse
+import argparse
 
 
 def get_text():
-    parser = argparse.ArgumentParser(prog="ceaser cipher encryption", description="this program takes a message and a postive or negative shift value for encryption and decryption")
-    parser.add_argument("-e", "--encrypt",  type=str,dest="message_encrypt", help="specify a positive value for encryption,eg:3")
-    parser.add_argument("-d", "--decrypt",  type=str,dest="message_decrypt", help="specify a negative value for decryption,eg:-3")
-    parser.add_argument("-s", "--shift",  type=int,dest="shift", help="specify -s for decryption")
+    program_name = "Caesar cipher encrypt/decrypt"
+    parser = argparse.ArgumentParser(prog=program_name, description="Encryption and decryption using the Caesar cipher")
+    parser.add_argument("-e", "--encrypt",  type=str, dest="message_encrypt", help="Specify a value for encryption")
+    parser.add_argument("-d", "--decrypt",  type=str, dest="message_decrypt", help="Specify a value for decryption")
+    parser.add_argument("-s", "--shift",  type=int, dest="shift", help="Specify a shift value")
     args = parser.parse_args()
     return args
 
 
-def ceaser(text, shift_value):
-    list = []
-    if text.isalpha():
-        for element in text:
+def caesar(text, shift_value):
+    list_value = []
+    result = ""
+    for element in text:
+        if element.isalpha():
             if element.isupper():
-                first_value = 65 # ascii offset value
-                ascii_value = ord(element) # convert letter to its corresponding ASCII
-                index_value = ascii_value - first_value
-                shifted_value_each_element = (index_value + shift_value) % 26 + first_value # z= (26+3)% 26 +65
-                list.append(shifted_value_each_element)
-
+                first_value = 65  # ASCII offset for uppercase letters
+                ascii_value = ord(element)  # Convert letter to its corresponding ASCII value
+                index_value = ascii_value - first_value  # Get the index of the letter in the alphabet (0-25)
+                shifted_value_each_element = (index_value + shift_value) % 26 + first_value  # shift operation
+                result += chr(shifted_value_each_element)  # Convert the shifted value back to a letter
             elif element.islower():
-                first_value = 97 # ascii offset value
+                first_value = 97  # ASCII offset for lowercase letters
                 ascii_value = ord(element)
                 index_value = ascii_value - first_value
-                shifted_value_each_element = (index_value + shift_value) % 26 + first_value  # z= (26+3)% 26 +65
-                list.append(shifted_value_each_element)
-
-        for element in list:
-            print(chr(element), end="")
-    else:
-        print("specify text (alphabets) only")
+                shifted_value_each_element = (index_value + shift_value) % 26 + first_value
+                result += chr(shifted_value_each_element)
+        else:
+            result += element  # Append non-alphabet characters unchanged
+    return result
 
 
 def main():
-    if cmd_line_input.message_decrypt:
-        if cmd_line_input.shift is not None:
-            ceaser(cmd_line_input.message_decrypt, cmd_line_input.shift)
-            exit()
-        print("\nspecify a SHIFT_PARAMETER, use --help to see help message")
-    elif cmd_line_input.message_encrypt:
-        if cmd_line_input.shift is not None:
-            ceaser(cmd_line_input.message_encrypt, cmd_line_input.shift)
-            exit()
-        print("\nspecify a SHIFT_PARAMETER, use --help to see help message")
+    args = get_text()
+    if args.message_decrypt:
+        if args.shift is not None:
+            decrypted_message = caesar(args.message_decrypt, -args.shift)  # Decryption
+            print(decrypted_message)
+        else:
+            print("\nSpecify a SHIFT_PARAMETER for decryption, use --help to see help message")
+    elif args.message_encrypt:
+        if args.shift is not None:
+            encrypted_message = caesar(args.message_encrypt, args.shift)  # Encryption
+            print(encrypted_message)
+        else:
+            print("\nSpecify a SHIFT_PARAMETER for encryption, use --help to see help message")
     else:
-        print("specify an argument, use --help to see help message")
+        print("Specify an argument, use --help to see help message")
 
 
-cmd_line_input = get_text()
-main()
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
